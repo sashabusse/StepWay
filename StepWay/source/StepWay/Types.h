1a2506cd665a45f4a50cc55cd596f6a9f163cf2e
sashabusse <sasha.busse@mail.ru>
2019-01-27 20:17:14 +0300

    first commit

---
+++ StepWay/source/StepWay/Types.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <cstdint>
+
+typedef char		byte;
+typedef int16_t		int16;
+typedef int32_t		int32;
+typedef int64_t		int64;
+
+
+typedef unsigned char	ubyte;
+typedef uint16_t		uint16;
+typedef uint32_t		uint32;
+typedef uint64_t		uint64;
+
+
+//fast
+typedef int_fast8_t		bytef;
+typedef int_fast16_t	int16f;
+typedef int_fast32_t	int32f;
+typedef int_fast64_t	int64f;
+
+
+typedef uint_fast8_t	ubytef;
+typedef uint_fast16_t	uint16f;
+typedef uint_fast32_t	uint32f;
+typedef uint_fast64_t	uint64f;
